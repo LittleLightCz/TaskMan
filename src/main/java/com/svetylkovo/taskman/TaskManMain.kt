52902399217849ba9d0a0e92b6a7a45f9e577c47
LittleLightCz <antonin.brettsnajdr@gmail.com>
2018-01-03 20:12:21 +0100

    H2 + Hibernate

--- src/main/java/com/svetylkovo/taskman/TaskManMain.kt
+++ src/main/java/com/svetylkovo/taskman/TaskManMain.kt
@@ -1,23 +1,45 @@
 package com.svetylkovo.taskman
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.svetylkovo.taskman.entity.Task
+import org.hibernate.Session
+import org.hibernate.cfg.Configuration
+import org.hibernate.service.ServiceRegistryBuilder
 import spark.Spark.*
 import java.net.InetAddress
 
+
 object TaskManMain {
 
-    data class Test(val name: String, val age: Int)
+    private val mapper = ObjectMapper()
 
     @JvmStatic
     fun main(args: Array<String>) {
+        obtainHibernateSession()?.let { session ->
+            staticFiles.location("/public")
+            port(7900)
 
+            get("/api/hostname") { request, response ->
+                InetAddress.getLocalHost().getHostName()
+            }
 
-        staticFiles.location("/public")
-        port(7900)
+            get("/api/tasks") { request, response ->
+                val tasks = session.createCriteria(Task::class.java).list()
+                mapper.writeValueAsString(tasks)
+            }
+        }
+    }
 
-        get("/api/hostname") { request, response ->
-            InetAddress.getLocalHost().getHostName()
+    private fun obtainHibernateSession(): Session? {
+        Class.forName("org.h2.Driver")
+
+        val configuration = Configuration().apply {
+            configure()
         }
 
+        val serviceRegistry = ServiceRegistryBuilder().applySettings(configuration.properties).buildServiceRegistry()
+        val sessionFactory = configuration.buildSessionFactory(serviceRegistry)
 
+        return sessionFactory.openSession()
     }
 }
---
+++ src/main/java/com/svetylkovo/taskman/entity/Task.kt
@@ -0,0 +1,20 @@
+package com.svetylkovo.taskman.entity
+
+import java.util.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long = 0
+    var name: String = ""
+    var detail: String? = null
+    var priority: Int = 0
+    var createdDate: Date? = null
+    var completedDate: Date? = null
+    var deleted: Boolean = false
+}
