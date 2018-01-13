ce1c39bed9c94cccbece1f265d7201a29530c043
LittleLightCz <antonin.brettsnajdr@gmail.com>
2018-01-13 14:12:51 +0100

    removed junk code

--- src/main/java/com/svetylkovo/taskman/TaskManMain.kt
+++ src/main/java/com/svetylkovo/taskman/TaskManMain.kt
@@ -23,20 +23,6 @@ object TaskManMain {
 
         ScheduledMaintanenceTask()
 
-//
-//        for (i in (1..100)) {
-//            val task = Task().apply {
-//                name = "name $i"
-//                detail = "detail $i"
-//                createdDate = Date()
-//                completedDate = Date(System.currentTimeMillis() - (i * 1000 * 60 * 60 * 5))
-//            }
-//
-//            session.beginTransaction()
-//            session.persist(task)
-//            session.transaction.commit()
-//        }
-
         launchDefaultBrowser(serverPort)
     }
 
