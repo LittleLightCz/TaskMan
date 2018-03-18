07c24bc599f3c27e251bdaa3d052131d2aeb8eec
LittleLightCz <antonin.brettsnajdr@gmail.com>
2018-03-18 13:51:21 +0100

    Gradual conversion of TasksController

--- src/main/java/com/svetylkovo/taskman/TaskMan.kt
+++ src/main/java/com/svetylkovo/taskman/TaskMan.kt
@@ -1,17 +1,30 @@
 package com.svetylkovo.taskman
 
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.svetylkovo.taskman.controller.TasksController.Companion.tasksController
+import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.content.defaultResource
 import io.ktor.content.resources
 import io.ktor.content.static
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
+import io.ktor.http.HttpStatusCode
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.jetty.Jetty
+import org.apache.commons.lang3.exception.ExceptionUtils
+import org.jboss.logging.Logger
 import spark.Spark.staticFiles
 import java.awt.Desktop
 import java.net.URI
 
 
 object TaskMan {
+
+    val log = Logger.getLogger(TaskMan::class.java)
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -21,15 +34,28 @@ object TaskMan {
 //        TasksController()
 //
 //        ScheduledMaintanenceTask()
-//
-//        launchDefaultBrowser(serverPort)
 
         val server = embeddedServer(Jetty, serverPort) {
+            install(ContentNegotiation) {
+                jackson {
+                    configure(SerializationFeature.INDENT_OUTPUT, true)
+                }
+            }
+
+            install(StatusPages) {
+                exception<Throwable> { cause ->
+                    log.error("Error occured!", cause)
+                    call.respond(HttpStatusCode.InternalServerError, ExceptionUtils.getStackTrace(cause))
+                }
+            }
+
             routing {
                 static {
                     resources("build")
                     defaultResource("build/index.html")
                 }
+
+                tasksController()
             }
         }
 
--- src/main/java/com/svetylkovo/taskman/controller/TasksController.kt
+++ src/main/java/com/svetylkovo/taskman/controller/TasksController.kt
@@ -2,139 +2,161 @@ package com.svetylkovo.taskman.controller
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.svetylkovo.taskman.entity.Task
 import com.svetylkovo.taskman.session.HibernateSessionFactory.obtainHibernateSession
 import com.svetylkovo.taskman.session.transaction
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode.*
+import io.ktor.http.HttpStatusCode.Companion.OK
+import io.ktor.request.receive
+import io.ktor.request.receiveMultipart
+import io.ktor.request.receiveParameters
+import io.ktor.request.receiveText
+import io.ktor.response.*
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.routing.header
+import io.ktor.routing.post
 import org.apache.commons.lang3.exception.ExceptionUtils
-import spark.Spark.*
+import spark.Spark.post
+
 import java.net.InetAddress
 import java.util.*
 
 
+
 class TasksController {
 
-    private val session = obtainHibernateSession()
-    private val mapper = ObjectMapper()
+
+    companion object {
+        private val session = obtainHibernateSession()
+
+        fun Routing.tasksController() {
+            get("/api/hostname") {
+                call.respond(InetAddress.getLocalHost().hostName)
+            }
+
+            get("/api/tasks") {
+                val tasks = session.createCriteria(Task::class.java).list()
+                call.respond(tasks)
+            }
+
+            post("/api/tasks/new") {
+                val task = call.receive<Task>().apply {
+                    createdDate = Date()
+                }
+
+                session.transaction {
+                    persist(task)
+                }
+
+                call.respond(OK)
+            }
+
+            post("/api/tasks/update") {
+                val task = call.receive<Task>()
+                updateTask(task.id) {
+                    it.name = task.name
+                    it.detail = task.detail
+                    it.priority = task.priority
+                }
+                call.respond(OK)
+            }
+
+            get("/api/tasks/export") {
+                val tasks = session.createCriteria(Task::class.java)
+                    .list()
+                    .filterIsInstance<Task>()
+                    .filter { it.completedDate == null }
+
+                with(call) {
+                    response.header("Content-Disposition", "attachment; filename=Tasks.json")
+                    respond(tasks)
+                }
+            }
+
+            post("/api/tasks/import") {
+                val json = call.receiveText()
+                val tasks = jacksonObjectMapper().readValue<List<Task>>(json)
+
+                session.transaction {
+                    tasks.forEach {
+                        it.id = 0
+                        persist(it)
+                    }
+                }
+
+                call.respond(OK)
+            }
+
+        }
+
+        private fun updateTask(id: Long, action: (Task) -> Unit) {
+            session.transaction {
+                val task = get(Task::class.java, id) as Task
+                action(task)
+                update(task)
+            }
+        }
+
+
+    }
+
 
     init {
-        exception(Exception::class.java, { exception, _, response ->
-            exception.printStackTrace()
 
-            with(response) {
-                status(500)
-                type("application/json")
-                body(mapper.writeValueAsString(ExceptionUtils.getStackTrace(exception)))
-            }
-        })
 
-        get("/api/hostname") { _, _ ->
-            InetAddress.getLocalHost().hostName
-        }
 
-        get("/api/tasks") { _, _ ->
-            val tasks = session.createCriteria(Task::class.java).list()
-            mapper.writeValueAsString(tasks)
-        }
 
-        post("/api/tasks/new") { req, _ ->
-            val task = mapper.readValue(req.body(), Task::class.java).apply {
-                createdDate = Date()
-            }
+//
 
-            session.transaction {
-                persist(task)
-            }
-            "OK"
-        }
+//
 
-        post("/api/tasks/update") { req, _ ->
-            val task = mapper.readValue(req.body(), Task::class.java)
-            updateTask(task.id) {
-                it.name = task.name
-                it.detail = task.detail
-                it.priority = task.priority
-            }
-            "OK"
-        }
-
-        get("/api/tasks/export") { req, response ->
-            val tasks = session.createCriteria(Task::class.java)
-                .list()
-                .filterIsInstance<Task>()
-                .filter { it.completedDate == null }
-
-            response.raw().apply {
-                contentType = "application/octet-stream"
-                setHeader("Content-Disposition", "attachment; filename=Tasks.json")
-
-                outputStream.writer().use {
-                    it.write(mapper.writeValueAsString(tasks))
-                }
-            }
-        }
-
-        post("/api/tasks/import") { req, _ ->
-            val tasks: List<Task> = mapper.readValue(req.body(), object : TypeReference<List<Task>>() {})
-
-            session.transaction {
-                tasks.forEach {
-                    it.id = 0
-                    persist(it)
-                }
-            }
-
-            "OK"
-        }
-
-        post("/api/task/delete/:id") { req, _ ->
-            session.transaction {
-                val id = req.params(":id")
-                val task = get(Task::class.java, id.toLong()) as Task
-                delete(task)
-            }
-            "OK"
-        }
+//
+//        post("/api/task/delete/:id") { req, _ ->
+//            session.transaction {
+//                val id = req.params(":id")
+//                val task = get(Task::class.java, id.toLong()) as Task
+//                delete(task)
+//            }
+//            "OK"
+//        }
 
         /**
          * Update routes
          */
 
-        updateTaskWhen("/api/task/done/:id") {
-            it.completedDate = Date()
-        }
-
-        updateTaskWhen("/api/task/escalate/:id") { task ->
-            if (task.priority > 1) task.priority--
-        }
-
-        updateTaskWhen("/api/task/deescalate/:id") { task ->
-            if (task.priority < 5) task.priority++
-        }
-
-        updateTaskWhen("/api/task/suspend/:id") { it.suspended = true }
-
-        updateTaskWhen("/api/task/unsuspend/:id") { it.suspended = false }
-
-        updateTaskWhen("/api/task/restore/:id") {
-            it.createdDate = Date()
-            it.completedDate = null
-        }
+//        updateTaskWhen("/api/task/done/:id") {
+//            it.completedDate = Date()
+//        }
+//
+//        updateTaskWhen("/api/task/escalate/:id") { task ->
+//            if (task.priority > 1) task.priority--
+//        }
+//
+//        updateTaskWhen("/api/task/deescalate/:id") { task ->
+//            if (task.priority < 5) task.priority++
+//        }
+//
+//        updateTaskWhen("/api/task/suspend/:id") { it.suspended = true }
+//
+//        updateTaskWhen("/api/task/unsuspend/:id") { it.suspended = false }
+//
+//        updateTaskWhen("/api/task/restore/:id") {
+//            it.createdDate = Date()
+//            it.completedDate = null
+//        }
 
     }
 
-    private fun updateTaskWhen(postPath: String, taskMutation: (Task) -> Unit) {
-        post(postPath) { req, _ ->
-            val id = req.params(":id")
-            updateTask(id.toLong(), taskMutation)
-            "OK"
+    private fun Routing.updateTaskWhen(postPath: String, taskMutation: (Task) -> Unit) {
+        post(postPath) {
+            call.parameters["id"]?.let { id -> updateTask(id.toLong(), taskMutation) }
+            call.respond(OK)
         }
     }
 
-    private fun updateTask(id: Long, action: (Task) -> Unit) {
-        session.transaction {
-            val task = get(Task::class.java, id) as Task
-            action(task)
-            update(task)
-        }
-    }
 }
