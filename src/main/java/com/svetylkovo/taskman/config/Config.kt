fdc2e3a6c8f810a5ef8caa2625deaa4627c7fc2c
LittleLightCz <antonin.brettsnajdr@gmail.com>
2018-03-21 18:52:04 +0100

    Updated frontend dependencies, used jacksonObjectMapper()

--- src/main/java/com/svetylkovo/taskman/config/Config.kt
+++ src/main/java/com/svetylkovo/taskman/config/Config.kt
@@ -1,21 +1,21 @@
 package com.svetylkovo.taskman.config
 
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.io.File
 
 
 object Config {
     private val configFileName = "TaskMan.json"
-    private val mapper = ObjectMapper().apply {
+    private val mapper = jacksonObjectMapper().apply {
         enable(SerializationFeature.INDENT_OUTPUT)
     }
 
     val taskmanConfiguration by lazy {
         val configFile = File(configFileName)
 
-        if (configFile.exists()) mapper.readValue(configFile, object : TypeReference<TaskManConfiguration>() {})
+        if (configFile.exists()) mapper.readValue(configFile)
         else TaskManConfiguration().also {
             mapper.writeValue(configFile, it)
         }
